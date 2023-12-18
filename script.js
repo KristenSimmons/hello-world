d0354b79045d2cfd0c36ddab36a51005d36a35db
Kristen Simmons <kristen_simmons@ymail.com>
2023-12-18 14:04:57 -0600

    updating

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 'use strict';
 
-console.log("My first intro into JavaScript!")
-console.log("Send help!")
+console.log("I am in the HTML");
 
-alert("Welcome to our company page!")
+alert("Welcome to our company page!");
 
-const userSrc = prompt("How did you hear about us?")
+const userSrc = prompt("How did you hear about us?");
 console.log("userSrc");
 
-
-
 if (userSrc == "google"){
-    alert (Thank you for using Google to find us!);
+    alert ("Thank you for using Google to find us!");
 } else if (userSrc == "referal"){
     alert ("Let us know who referred you so we can thank them!");
 } else if (userSrc == "drive by"){
     alert ("Glad you found us!"); 
 } else {
-    alert (Glad you found us! Check out our amzing reviews!);
+    alert ("Glad you found us! Check out our amzing reviews!");
 }
 
-function rateThisPage(){
-    let rating = prompt("What do you rate this page on a scale of 1-5?");
-    for (let i = 0; i < rating; i++) {
-       document.write('<img src="images/Frolic.gif" class="rating-img" alt="Homer Simpson frolicing in a meadow gif">');
-    }
- 
-
-
-    
 
